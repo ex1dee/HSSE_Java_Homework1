7016ce3d99c2b5447ad13b799bdbfb74ed97b51a
Sergey Rezchikov <rez4.off@example.com>
2025-09-17 22:32:33 +0300

    fix: string comparison in Employee.java

--- src/main/java/com/mipt/rezchikovsergey/Application.java
+++ src/main/java/com/mipt/rezchikovsergey/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         for (int i = 0; i < 15; i++) {
-            System.out.println("Iter: " + (String) i)
+            System.out.println("Iter: " + i);
         }
     }
 }
--- src/main/java/com/mipt/rezchikovsergey/model/Employee.java
+++ src/main/java/com/mipt/rezchikovsergey/model/Employee.java
@@ -4,6 +4,6 @@ public abstract class Employee {
     public abstract void work(int anyInt);
 
     public boolean goHome(String str1, String str2) {
-        return str1 == str2;
+        return str1.equals(str2);
     }
 };
