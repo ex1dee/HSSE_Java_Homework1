a18e64b0285f7ee513ebc36debca8dea95bfc12b
Sergey Rezchikov <rez4.off@example.com>
2025-10-11 20:23:28 +0300

    feat: add classes with generics

---
+++ src/main/java/com/mipt/rezchikovsergey/generics/Pair.java
@@ -0,0 +1,35 @@
+package com.mipt.rezchikovsergey.generics;
+
+public class Pair<K, V> {
+  private K key;
+  private V value;
+
+  public Pair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public Pair<V, K> swap() {
+    return new Pair<>(value, key);
+  }
+
+  public String toString() {
+    return "Pair{key=" + key + ", value=" + value + "}";
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public void setKey(K key) {
+    this.key = key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  public void setValue(V value) {
+    this.value = value;
+  }
+}
