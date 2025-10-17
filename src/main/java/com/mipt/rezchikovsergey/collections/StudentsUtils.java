4f27341c12dbb8f8ebf15970acc48fc448f1ad7f
Sergey Rezchikov <rez4.off@example.com>
2025-10-17 21:17:54 +0300

    feat: - add custom list - add performance comparison between ArrayList and LinkedList - add student utils

---
+++ src/main/java/com/mipt/rezchikovsergey/collections/StudentsUtils.java
@@ -0,0 +1,32 @@
+package com.mipt.rezchikovsergey.collections;
+
+import java.util.*;
+
+public class StudentsUtils {
+  public static List<Student> findStudentsByGradeRange(Map<Integer, Student> map, double minGrade, double maxGrade) {
+    List<Student> students = new ArrayList<>();
+
+    for (Map.Entry<Integer, Student> studentEntry : map.entrySet()) {
+      Student student = studentEntry.getValue();
+
+      if (student.grade >= minGrade && student.grade <= maxGrade) {
+        students.add(student);
+      }
+    }
+
+    return students;
+  }
+
+  public static List<Student> getTopNStudents(TreeMap<Integer, Student> map, int n) {
+    List<Student> students = new ArrayList<>();
+
+    for (Map.Entry<Integer, Student> studentEntry : map.reversed().entrySet()) {
+      if (students.size() == n)
+        break;
+
+      students.add(studentEntry.getValue());
+    }
+
+    return students;
+  }
+}
--- src/main/java/com/mipt/rezchikovsergey/model/Employee.java
+++ src/main/java/com/mipt/rezchikovsergey/model/Employee.java
@@ -1,9 +1,9 @@
 package com.mipt.rezchikovsergey.model;
 
 public abstract class Employee {
-    public abstract void work(int anyInt);
+  public abstract void work(int anyInt);
 
-    public boolean goHome(String str1, String str2) {
-        return str1.equals(str2);
-    }
-};
+  public boolean goHome(String str1, String str2) {
+    return str1.equals(str2);
+  }
+}
