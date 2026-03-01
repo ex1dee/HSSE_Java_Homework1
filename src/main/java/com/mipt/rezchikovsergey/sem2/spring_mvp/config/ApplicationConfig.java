e4d0a211445d67e1370a464b12323d71c8cc3259
cmetanna <rez4.off@gmail.com>
2026-03-02 00:10:22 +0300

    fix: wrong stubTaskRepository bean creation in ApplicationConfig

--- src/main/java/com/mipt/rezchikovsergey/sem2/spring_mvp/config/ApplicationConfig.java
+++ src/main/java/com/mipt/rezchikovsergey/sem2/spring_mvp/config/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package com.mipt.rezchikovsergey.sem2.spring_mvp.config;
 
-import com.mipt.rezchikovsergey.sem2.spring_mvp.repository.InMemoryTaskRepository;
+import com.mipt.rezchikovsergey.sem2.spring_mvp.repository.StubTaskRepository;
 import com.mipt.rezchikovsergey.sem2.spring_mvp.repository.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
   @Bean
   public TaskRepository stubTaskRepository() {
-    return new InMemoryTaskRepository();
+    return new StubTaskRepository();
   }
 }
